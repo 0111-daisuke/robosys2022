b45912db20cd2101fa1e8f4a8d22d2b4ffc8e1f5
0111-daisuke <s21C1127lm@s.chibakoudai.jp>
2022-10-27 12:21:03 +0900

    add a command

--- test.bash
+++ test.bash
@@ -2,6 +2,17 @@
 #SPDX-FileCopyrightText: 2022 Daisuke Mori
 #SPDX-License-Identifier: BSD-3-clause
 
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+rse=0
+
+### I/O TEST ###
 out=$(seq 5 |./plus)
 
-[ "${out}" = 15 ]
+[ "${out}" = 14 ] || ng ${LINEDO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
